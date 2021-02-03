5986e93d12eb7ebfccba430d4be4b3c34dee7b18
Otto Hrubý <otto.hruby@gmail.com>
2021-02-03 10:23:48 +0100

    added ThreadedServer class

--- server.py
+++ server.py
@@ -1,49 +1,77 @@
+
 #python3
 import socket
-import os
 import time
-import csv
-from _thread import *
 from config import *
+import threading
 
-ServerSocket = socket.socket()
-host = '0.0.0.0'
-port = 61000
-ThreadCount = 0
-try:
-    ServerSocket.bind((host, port))
-except socket.error as e:
-    print(str(e))
+def ParsePLCResponse(aData):
+    # read and split data from client
+    # first: error no., second: model, third: process no.
+    errorNo = int.from_bytes(aData[0:2], byteorder='little', signed=False)
+    processNo = int.from_bytes(aData[2:4], byteorder='little', signed=False)
+    modelNo = int.from_bytes(aData[4:6], byteorder='little', signed=False)
+    print(f"Parsed data- ErrorNo: {errorNo}; ProcessNo: {processNo}; ModelNo: {modelNo}")
+    return errorNo,processNo,modelNo
 
-print('Waitiing for a Connection..')
-ServerSocket.listen(5)
 
-def threaded_client(connection):
-    #connection.send(str.encode('Welcome to the Server\n'))
-    while True:
-        data = connection.recv(512)
-        LineOfLog = CreateRow(Get_Err(data))
-        WriteRow(LineOfLog)
-        time.sleep(0.1)
-        #read and split data from client
-        #first: error no., second: model, third: process no.
-        reply = 'Server Says: ' + data.decode('utf-8')
-        if not data:
-            break
-        connection.send(str.encode(reply))
-    connection.close()
-#
-#def write_log(process,data):
+class ThreadedServer():
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.ServerSocket = socket.socket()
 
-while True:
-    try:
-        Client, address = ServerSocket.accept()
-        print('Connected to: ' + address[0] + ':' + str(address[1]))
-        start_new_thread(threaded_client, (Client, ))
-        ThreadCount += 1
-        print('Thread Number: ' + str(ThreadCount))
-    except socket.error as e:
-        print(str(e))
-        time.sleep(2)
+        try:
+            self.ServerSocket.bind((host, port))
+            print('Listening at port:',port)
+        except socket.error as e:
+            print(str(e))
 
-ServerSocket.close()
+    def listen(self):
+        print('Waiting for a Connection..')
+        self.ServerSocket.listen(5)
+        while True:
+            try:
+                client, address = self.ServerSocket.accept()
+                print(f"Connected to: {address[0]}:{address[1]}")
+                threading.Thread(target=self.listenClient, args=(client,)).start()
+                print(f"Number active of threads: {threading.active_count()}")
+            except socket.error as e:
+                print(str(e))
+                time.sleep(2)
+        self.ServerSocket.close()
+
+    def listenClient(self, connection):
+        while True:
+            try:
+                data = connection.recv(12)
+            except socket.error:
+                print("Could not receive data from PLC")
+                break
+
+            print(f"Received data: {data}")
+            data_parsed = ParsePLCResponse(data)  # words to tuple (ErrorNo,ProcessNo,ModelNo)
+
+            try:
+                LineOfLog = CreateRow(Get_Err(data_parsed[0]))
+                WriteRow(LineOfLog)
+            except Exception as e:
+                print(e)
+                time.sleep(0.1)
+
+            reply = int.to_bytes(123, length=6, byteorder='little', signed=False)  # debug
+            if not data:
+                break
+            print(f"Server response: {reply}")
+            connection.send(reply)
+        connection.close()
+
+
+if __name__ == "__main__":
+    HOST = '0.0.0.0'
+    PORT = 4200
+
+    ThreadedServer(HOST,PORT).listen()
+
+
+
