0a78ec180b75a8c5618cf80d9cc3c7cf15160c85
Otto Hrubý <otto.hruby@gmail.com>
2021-02-25 09:57:47 +0100

    added logging

--- server.py
+++ server.py
@@ -3,6 +3,9 @@ import socket
 import time
 from config import *
 import threading
+import logging
+#Initialization of logging into log fie
+logging.basicConfig(filename='sys_log/'+GetDate("file")+'.log',encoding='utf-8', level=logging.DEBUG, format='%(asctime)s %(message)s')
 
 def ParsePLCResponse(aData):
     # read and split data from client
@@ -21,30 +24,39 @@ class ThreadedServer():
         self.ServerSocket = socket.socket()
 
         try:
+            #self.ServerSocket(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
             self.ServerSocket.bind((host, port))
+            logging.info("Socket was binded %s",port)
         except socket.error as e:
+            logging.error(e,exc_info=True)
             print(str(e))
 
     def listen(self):
         print('Waiting for a Connection..')
+        logging.info("Waiting for connection")
         self.ServerSocket.listen(5)
         while True:
             try:
                 client, address = self.ServerSocket.accept()
                 print(f"Connected to: {address[0]}:{address[1]}")
                 threading.Thread(target=self.listenClient, args=(client,)).start()
+                logging.info("New connection added %s",client)
                 print(f"Number active of threads: {threading.active_count()}")
                 print(f"List of threads: {threading.enumerate()}")
             except socket.error as e:
                 print(str(e))
                 time.sleep(2)
+                logging.error(e, exc_info=True)
         self.ServerSocket.close()
 
     def listenClient(self, connection):
         while True:
             try:
-                data = connection.recv(12)
+                data = connection.recv(6)
+                logging.info("Received data: %s Lenght of data: %s",data,len(data))
             except socket.error:
+                logging.info("Could not received data from PLC, given data %s",data)
+                logging.error(socket.error)
                 print("Could not receive data from PLC")
                 break
             #print(f"Received data: {data}")
@@ -54,17 +66,23 @@ class ThreadedServer():
                 LineOfLog = CreateRow(Get_Err(data_parsed[0],data_parsed[1]))
                 WriteRow(LineOfLog, data_parsed[1])
                 process = Get_Machine_From_Process(data_parsed[1])
-                print ("Process"+str(process)+" had error: "+LineOfLog)
+                print (str(process)+" had error: "+LineOfLog)
             except Exception as e:
+                logging.info('Something happned in Config.py')
+                logging.error(e, exc_info=True)
                 print(e)
                 time.sleep(0.1)
 
             reply = int.to_bytes(123, length=6, byteorder='little', signed=False)  # debug
             if not data:
+                logging.info("Data was not correct, check data!")
+                logging.debug("Given data:%s",data)
                 break
             #print(f"Server response: {reply}")
             connection.send(reply)
+            logging.info("Reply from server to PLC was %s",reply)   
         connection.close()
+        logging.debug("Connection was closed")
 
 
 if __name__ == "__main__":
