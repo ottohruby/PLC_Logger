78204b1a464e0536633da1f478eafdb3f86212e1
Otto Hruby <otto.hruby@gmail.com>
2021-07-07 08:48:52 +0200

    Optimized function binary bytes to tuple

--- utils.py
+++ utils.py
@@ -15,7 +15,7 @@ def get_date(mode):
     elif mode == "folder":
         return today.strftime("%y%m")
     else:
-        raise ValueError(f"Invalid mode selected in get_date function: {mode}")
+        raise ValueError(f"Invalid mode selected in utils get_date function: {mode}")
 
 
 def binary_bytes_to_tuple(raw, length, byteorder='little', signed=False):
@@ -30,8 +30,8 @@ def binary_bytes_to_tuple(raw, length, byteorder='little', signed=False):
     """
 
     raw += b'\x00' * (length * 2 - len(raw))  # Fill data to fit specified length with zeros
+    return tuple(int.from_bytes(pair, byteorder=byteorder, signed=signed) for pair in zip(raw[::2], raw[1::2]))
 
-    return tuple(int.from_bytes(pair, byteorder=byteorder, signed=signed) for pair in list(zip(raw[::2], raw[1::2])))
 
 
 def binary_string_to_tuple(raw, length, delimiter=None, encoding='ascii'):
